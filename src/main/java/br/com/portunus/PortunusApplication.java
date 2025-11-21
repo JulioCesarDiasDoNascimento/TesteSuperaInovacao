32c2e502cd6e1ff35f23ed6c00df98d5c8472749
Júlio Dias <julio.dias@trizy.com.br>
2025-11-20 22:36:46 -0300

    main - enviando configurações iniciais do projeto

---
+++ src/main/java/br/com/portunus/PortunusApplication.java
@@ -0,0 +1,13 @@
+package br.com.portunus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortunusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortunusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/portunus/PortunusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portunus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortunusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
